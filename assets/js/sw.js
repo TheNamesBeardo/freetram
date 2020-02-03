a2f1c738b6c6853242dc566cdfdda88e1858e3cf
TheNamesBeardo <thenamesbeardo@gmail.com>
2020-02-03 21:00:05 +0000

    Update sw.js

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,1 +1,12 @@
-
+const cacheName = 'tram-v1'
+const staticAssets = [
+  './'
+];
+self.addEventListener('install', async e {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
+});
+self.addEventListener('activate', e {
+  self.clients.claim();
+});
